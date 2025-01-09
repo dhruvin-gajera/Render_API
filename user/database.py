9921666407d6d46f08f1ab3941296e3ec1da143f
Dhruvin Gajera <90386431+dhruvin-gajera@users.noreply.github.com>
2025-01-09 17:28:15 +0530

    Update database.py

--- user/database.py
+++ user/database.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 
 # breakpoint() 
 
-URL_DATABASE = "postgresql://postgres:Admin@localhost:5432/amb"
+URL_DATABASE = "postgresql://competitionuser:YdfWB4R9xpgfnXx8qKlfHhTvcve5VDRV@dpg-ctvrhorv2p9s7398d650-a.oregon-postgres.render.com/competition_ftzg"
 
 engine = create_engine(URL_DATABASE)
 
